b31644c85b1db0dd9c08b5958c6c7bab5bccf0ac
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2026-01-15 11:53:23 +0200

    Create new branch

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { EntityNotFoundErrorFilter } from './entity-not-found-error.filter';
 
+// This is a dev branch
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
