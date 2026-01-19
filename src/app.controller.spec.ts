dcf27268a85d6602d78713a89f7beaad3546d54e
YuriiStepaniuk <yurii.stepaniuk.dev@gmail.com>
2026-01-19 13:39:53 +0200

    Test Dockerfile

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -13,8 +13,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World', () => {
-      expect(appController.getHello()).toBe('Hello World! NEW"');
+    it('should return "Hello World"', () => {
+      expect(appController.getHello()).toBe('Hello World! NEW');
     });
   });
 });
